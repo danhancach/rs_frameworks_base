1fe6b2d604e06e0776f052301b9ffb755866c1f4
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-09 15:47:54 +0800

    SystemUI: Fix QS column count on landscape mode
    * drop updatecolumns in favor of keeping aosp behaviour
    
    Change-Id: I2e87af35c48b4b503e215fe6c21d2ac87b4bbecd
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanelController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanelController.java
@@ -94,7 +94,6 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
     protected void onInit() {
         super.onInit();
         updateMediaExpansion();
-        mView.updateColumns();
         mMediaHost.setShowsOnlyActiveMedia(true);
         mMediaHost.init(MediaHierarchyManager.LOCATION_QQS);
         mBrightnessSliderController.init();
@@ -129,8 +128,6 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
             mView.updateResources();
             updateBrightnessMirror();
         });
-        
-        mView.updateColumns();
 
         mBrightnessMirrorHandler.onQsPanelAttached();
     }
@@ -169,7 +166,6 @@ public class QuickQSPanelController extends QSPanelControllerBase<QuickQSPanel> 
     private void setMaxTiles(int parseNumTiles) {
     	if (mView == null) return;
         mView.setMaxTiles(parseNumTiles);
-        mView.updateColumns();
         setTiles();
     }
 
